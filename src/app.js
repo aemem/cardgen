10d0d0b99b54c2c0faa33a9c982aa3507f1a78e0
aemem <anamoliner.marques@gmail.com>
2022-03-27 22:21:09 +0000

    first

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const diamonds = "♦";
+const hearts = "♥";
+const spades = "♠";
+const clubs = "♣";
+
+const suit = [diamonds, hearts, spades, clubs];
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const baraja = [];
+
+//generar baraja
+const genBaraja = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      const numero = arr1[i];
+      const simbolo = arr2[j];
+      baraja.push({ numero, simbolo });
+    }
+  }
+  return baraja;
+};
+
+//generar carta
+const genCard = baraja => {
+  const random = Math.floor(Math.random() * baraja.length);
+  const genNum = baraja[random].numero;
+  const genSuit = baraja[random].simbolo;
+  document.querySelector(".cartaNum").innerHTML = genNum;
+  document.querySelector(".cartaTop").innerHTML = document.querySelector(
+    ".cartaBottom"
+  ).innerHTML = genSuit;
+};
+
+//boton random
+cartaRandom.addEventListener("click", randomClick);
+
+const randomClick = baraja => {
+  const random = Math.floor(Math.random() * baraja.length);
+  const genNum = baraja[random].numero;
+  const genSuit = baraja[random].simbolo;
+  document.querySelector(".cartaNum").innerHTML = genNum;
+  document.querySelector(".cartaTop").innerHTML = document.querySelector(
+    ".cartaBottom"
+  ).innerHTML = genSuit;
+};
+
+//boton reset
+reset.addEventListener("click", resetBaraja);
+
+const resetBaraja = baraja => {};
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  genBaraja(num, suit);
+  genCard(baraja);
 };
